49c4f3dde49fb32fbfb8a4f66943d381010230ef
Rashmi <rashmi.manandhar7@gmail.com>
2020-05-02 16:49:41 -0400

    feat: hook redux to the application

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import reducer from "./reducers";
+
 // @ts-ignore
 import AddEntry from './components/AddEntry';
 
@@ -11,9 +15,11 @@ export default class App extends Component {
 
   render() {
     return (
+      <Provider store={createStore(reducer)}>
       <View>
         <AddEntry/>
       </View>
+      </Provider>
     );
   }
 
