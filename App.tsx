5d3e138da95643ce846f36f6c0c1cda63be3fec1
Rashmi <rashmi.manandhar7@gmail.com>
2020-05-03 11:06:21 -0400

    feat: create a function to fetch all calendar results

--- App.tsx
+++ App.tsx
@@ -1,29 +1,19 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
-import {createStore} from "redux";
-import {Provider} from "react-redux";
-// @ts-ignore
-import reducer from './reducers';
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
 
-// @ts-ignore
-import AddEntry from './components/AddEntry';
-
-export default class App extends Component {
-  componentDidMount() {
-    console.log("before");
-    console.log("after");
-  }
-
+export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-      <View style={{flex: 1}}>
-        <AddEntry/>
-      </View>
+        <View style={{flex: 1}}>
+          <History />
+        </View>
       </Provider>
-    );
+    )
   }
-
-
 }
-
