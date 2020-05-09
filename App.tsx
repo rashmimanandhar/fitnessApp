a59adf3ff7dffee3af8ab6a6f7bb73981f26306f
Rashmi <rashmi.manandhar7@gmail.com>
2020-05-09 11:22:21 -0400

    feat: add navigation to the app

--- App.tsx
+++ App.tsx
@@ -5,15 +5,60 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import {FontAwesome, Ionicons} from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={createStore(reducer)}>
-        <View style={{flex: 1}}>
-          <History />
-        </View>
+import {purple, white} from "./utils/colors";
+
+const Tabs =
+  Platform.OS === "ios"
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator();
+
+export default class App extends React.Component{
+  render(){
+    const store = createStore(reducer)
+    return(
+      <Provider store={store}>
+        <NavigationContainer>
+          <Tabs.Navigator
+            initialRouteName="AddEntry"
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let icon;
+                if (route.name === "Add Entry") {
+                  icon = (
+                    <FontAwesome name="plus-square" size={size} color={color} />
+                  );
+                } else if (route.name === "History") {
+                  icon = (
+                    <Ionicons name="ios-bookmarks" size={size} color={color} />
+                  );
+                }
+                return icon;
+              }
+            })}
+            tabBarOptions={{
+              activeTintColor: Platform.OS === "ios" ? purple : white,
+              style: {
+                height: 80,
+                backgroundColor: Platform.OS === "ios" ? white : purple,
+                shadowColor: "rgba(0, 0, 0, 0.24)",
+                shadowOffset: {
+                  width: 0,
+                  height: 3
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1
+              }
+            }}
+          >
+            <Tabs.Screen name="Add Entry" component={AddEntry} />
+            <Tabs.Screen name="History" component={History} />
+          </Tabs.Navigator>
+        </NavigationContainer>
       </Provider>
     )
   }
